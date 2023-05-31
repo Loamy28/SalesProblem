7dba62d538612026c11076f4dbf89dbcf79276f4
Rome Angelo Gagabi <rome.angelo15@gmail.com>
2023-05-31 17:00:46 +0800

    Commit

--- LongestDynasty.js
+++ LongestDynasty.js
@@ -1,11 +1,11 @@
 const dynastyReign = [
-    { name: "San Dynasty", year: "MXLI", },
-    { name: "Viloria Dynasty", year: "MCCCIIII", },
-    { name: "Tan Dynasty", year: "MCCCXCVIII", },
-    { name: "Bon Dynasty", year: "MCDXLV", },
-    { name: "Maiko Dynasty", year: "MDCLXIV", },
-    { name: "Paul Dynasty", year: "MCMXLIX", },
-    { name: "Andre Dynasty", year: "MMMXICX", },
+    { name: "San Dynasty", year: "MXLI" },
+    { name: "Viloria Dynasty", year: "MCCCIIII" },
+    { name: "Tan Dynasty", year: "MCCCXCVIII" },
+    { name: "Bon Dynasty", year: "MCDXLV" },
+    { name: "Maiko Dynasty", year: "MDCLXIV" },
+    { name: "Paul Dynasty", year: "MCMXLIX" },
+    { name: "Andre Dynasty", year: "MMMXICX" },
   ];
   
   const longestDynasty = () => {
@@ -19,7 +19,7 @@ const dynastyReign = [
     dynastyReign.forEach((currentDynasty) => {
       const endYear = convertYear(currentDynasty.year);
   
-      if (endYear === "Invalid") {
+      if (endYear === -1) {
         return;
       }
   
@@ -40,15 +40,49 @@ const dynastyReign = [
     let year = 0;
     for (let i = 0; i < Year.length; i++) {
       const currentSymbol = Year[i];
+      const currentSymbolValue = romanNumerals[currentSymbol];
       const nextSymbol = Year[i + 1];
+      const nextSymbolValue = romanNumerals[nextSymbol] || 0;
   
-      if (romanNumerals[currentSymbol] < romanNumerals[nextSymbol]) {
-        year -= romanNumerals[currentSymbol];
+      if (currentSymbolValue < nextSymbolValue) {
+        year -= currentSymbolValue;
       } else {
-        year += romanNumerals[currentSymbol];
+        year += currentSymbolValue;
       }
     }
+    
+    if (Year !== convertToRoman(year)) {
+      return -1;
+    }
+    
     return year;
+  };
+  
+  const convertToRoman = (number) => {
+    const romanNumerals = [
+      { value: 1000, symbol: "M" },
+      { value: 900, symbol: "CM" },
+      { value: 500, symbol: "D" },
+      { value: 400, symbol: "CD" },
+      { value: 100, symbol: "C" },
+      { value: 90, symbol: "XC" },
+      { value: 50, symbol: "L" },
+      { value: 40, symbol: "XL" },
+      { value: 10, symbol: "X" },
+      { value: 9, symbol: "IX" },
+      { value: 5, symbol: "V" },
+      { value: 4, symbol: "IV" },
+      { value: 1, symbol: "I" },
+    ];
+  
+    let roman = "";
+    for (const numeral of romanNumerals) {
+      while (number >= numeral.value) {
+        roman += numeral.symbol;
+        number -= numeral.value;
+      }
+    }
+    return roman;
   };
   
   console.log("Longest reigning dynasty:", longestDynasty());
