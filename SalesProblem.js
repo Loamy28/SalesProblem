4f01e6ff4df56fd7d93016cad0ff2abbfb6a2d59
Rome Angelo Gagabi <rome.angelo15@gmail.com>
2023-05-31 13:04:15 +0800

    Longest Dynasty added

--- SalesProblem.js
+++ SalesProblem.js
@@ -1,47 +1,42 @@
 const productProfitArray = [
-    { name: "Product A", profit: -75 },
-    { name: "Product B", profit: -70 },
-    { name: "Product C", profit: 93 },
-    { name: "Product D", profit: 5 },
-    { name: "Product E", profit: 88 },
-    { name: "Product F", profit: 29 },
-  ];
+  { name: "Product A", profit: -75 },
+  { name: "Product B", profit: -70 },
+  { name: "Product C", profit: 93 },
+  { name: "Product D", profit: 5 },
+  { name: "Product E", profit: 88 },
+  { name: "Product F", profit: 29 },
+];
+
+const topProduct = () => {
+  let top = { profit: -Infinity };
+  productProfitArray.forEach((product) => {
+    if (product.profit > top.profit) {
+      top = product;
+    }
+  });
   
-  const topProduct = () => {
+  console.log(`Top product = name: ${top.name}, profit: ${top.profit}`);
+};
+topProduct();
 
-    let top = { profit: -Infinity };
-    productProfitArray.forEach((product) => {
-      if (product.profit > top.profit) {
-        top = product;
-      }
+const bottomProduct = () => {
+  let bottom = { profit: Infinity };
+  productProfitArray.forEach((product) => {
+    if (product.profit < bottom.profit) {
+      bottom = product;
+    }
+  });
+  console.log(`Bottom product = name: ${bottom.name}, profit: ${bottom.profit}`);
+};
+bottomProduct();
 
-    });
-    console.log(`Top product = name: ${top.name}, profit: ${top.profit}`);
-  };
-
-  topProduct();
-  
-  const bottomProduct = () => {
-    let bottom = { profit: Infinity };
-    productProfitArray.forEach((product) => {
-      if (product.profit < bottom.profit) {
-        bottom = product;
-      }
-    });
-
-    console.log(`Bottom product = name: ${bottom.name}, profit: ${bottom.profit}`);
-  };
-
-  bottomProduct();
-  
-  const zeroProfitProduct = () => {
-    let zero = { profit: Infinity };
-    productProfitArray.forEach((product) => {
-      if (Math.abs(product.profit) < Math.abs(zero.profit)) {
-        zero = product;
-      }
-    });
-    console.log(`Zero Profit product = name: ${zero.name}, profit: ${zero.profit}`);
-  };
-  zeroProfitProduct();
-  
+const zeroProfitProduct = () => {
+  let zero = { profit: Infinity };
+  productProfitArray.forEach((product) => {
+    if (Math.abs(product.profit) < Math.abs(zero.profit)) {
+      zero = product;
+    }
+  });
+  console.log(`Zero Profit product = name: ${zero.name}, profit: ${zero.profit}`);
+};
+zeroProfitProduct();
